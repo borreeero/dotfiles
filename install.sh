d75de7f1e436bcae83253a65f4b5de91e3281046
Cristóbal Borrero <cristobaljb@gmail.com>
2023-03-31 23:59:10 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env sh
 
-# Install chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init --apply --ssh borreeero
-echo "Installation finished. You can delete this clone now, it is managed by chezmoi in ~/.local/share/chezmoi"
+# From https://github.com/chezmoi/dotfiles/blob/master/install.sh
+
+set -eu
+
+if ! chezmoi="$(command -v chezmoi)"; then
+  bin_dir="${HOME}/.local/bin"
+  chezmoi="${bin_dir}/chezmoi"
+  echo "Installing chezmoi to '${chezmoi}'" >&2
+  if command -v curl >/dev/null; then
+    chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
+  elif command -v wget >/dev/null; then
+    chezmoi_install_script="$(wget -qO- https://chezmoi.io/get)"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+  unset chezmoi_install_script bin_dir
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+# TODO possibly add --one-shot if running in a container
+set -- init --apply --source="${script_dir}"
+
+echo "Running 'chezmoi $*'" >&2
+# exec: replace current process with chezmoi
+exec "$chezmoi" "$@"
