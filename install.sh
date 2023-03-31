84bf9fe15f84d9fa5fea99bd49261567dcb23d79
Cristóbal Borrero <cristobaljb@gmail.com>
2023-03-31 17:22:35 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-BASEDIR=$(dirname "$0")
-
 # Install chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init "$BASEDIR"
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init --apply --ssh borreeero
+echo "Installation finished. You can delete this clone now, it is managed by chezmoi in ~/.local/share/chezmoi"
