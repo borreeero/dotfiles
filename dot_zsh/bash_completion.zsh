95867220c521a56f305ad6918bfff5776317745e
Cristóbal Borrero <cristobaljb@gmail.com>
2023-03-31 17:19:38 +0200

    minor fixes

--- dot_zsh/bash_completion.zsh
+++ dot_zsh/bash_completion.zsh
@@ -1,4 +1,5 @@
 # Local Bash completion
+test -d ${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/ || return
 for f in ${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/*; do
     source $f
 done
